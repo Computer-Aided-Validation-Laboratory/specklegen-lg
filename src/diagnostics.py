608f692816de0403aa06112afe3711a6d1b1c7ca
Lavya Ghanwat <lavya2629@gmail.com>
2026-08-28 12:57:04 +0100

    First Push
    Added the src folder, examples folder, output folder, input deck and the main.py file. 
    Output folder has some pre-run simulations and the examples folder has some example input decks.

---
+++ src/diagnostics.py
@@ -0,0 +1,125 @@
+import numpy as np
+import scipy.fft as fft
+import matplotlib.pyplot as plt
+import os
+
+class speckleDiagnostics():
+
+    def __init__(self, pattern, width, height, run):
+
+        """
+        speckleDiagnostics
+
+        Class to compute various diagnostics with the speckle pattern. 
+        Functions:
+        fourierTransformer      --> Computes a FFT for the speckle pattern. 
+        visualizeFFT            --> Plots the FFT for visualization
+
+        Inputs:
+        pattern                 --> Dictionary containing the speckle pattern. 
+        width, height           --> Dimensions specified by user.
+        run                     --> Run name.
+        """
+
+        #Assigning variables to self.
+        self.pattern = pattern
+        self.width = width
+        self.height = height
+        self.run = run
+
+        #Extracting arrays from pattern dictionary
+        f = self.pattern["f"]
+        nx = self.pattern["nx"]
+        ny = self.pattern["ny"]
+        dx = self.pattern["dx"]
+        dy = self.pattern["dy"]
+
+        #Fourier transform
+        self.amplitudes, self.f_x, self.f_y = self.fourierTransformer(f, nx, dx, ny, dy)
+
+
+    #### Fourier Transformer #####
+    def fourierTransformer(self, f, nx, dx, ny, dy):
+
+        """
+        fourierTransformer
+
+        Function to compute the FFT for the speckle pattern. 
+        Acts as a diagnostic to check average speckle choice although can get a bit inaccurate for tiny speckles. 
+
+        Inputs:
+        f       --> Speckle Pattern
+        nx      --> Number of x points
+        dx      --> Distance b/w x points (uniform)
+        ny      --> Number of y points
+        dy      --> Distance b/w y points (uniform)
+
+        Outputs:
+        amplitudes, frequencies
+        """
+
+        #Fourier transforming
+        fft_complex = fft.fft2(f)
+
+        #Get frequencies
+        freq_x = fft.fftfreq(nx, dx)
+        freq_y = fft.fftfreq(ny, dy)
+
+        mask_x = freq_x > 0
+        mask_y = freq_y > 0
+
+        f_x, f_y = np.meshgrid(freq_x[mask_x], freq_y[mask_y])
+
+        #Amplitudes
+        amplitudes = (1 / (nx * ny)) * np.abs(fft_complex)
+        amplitudes = amplitudes[mask_y,:]
+        amplitudes = amplitudes[:,mask_x]
+
+        #Sizes
+        f_x = (1/f_x) * dx 
+        f_y = (1/f_y) * dy
+
+        return amplitudes, f_x, f_y
+
+
+    #### FFT Visualizer ####
+    def visualizeFFT(self, visualize = True, save = True):
+
+        """
+        visualizeFFT
+
+        Function to visualize the computed FFT diagnostic. 
+        Inputs:
+        visualize   --> Flag to visualize the FFT
+        save        --> Flag to save the FFT
+
+        Outputs:
+        Saved FFT.
+        """
+
+        #Generate pretty figure
+        plt.figure(figsize=(10,10))
+        plt.title("Fast Fourier Transform of the Speckle Pattern")
+        plt.pcolormesh(self.f_x, self.f_y, self.amplitudes)
+        plt.colorbar()
+        plt.xscale("log")
+        plt.yscale("log")
+        plt.ylabel("y-pixel sizes (height)")
+        plt.xlabel("x-pixel sizes (width)")
+
+        #Control flows for saving.
+        if save:
+            #File names
+            directory = "output/"+self.run+"/"
+            save_path = directory+self.run+"_FFTDiagnostic.png"
+            os.makedirs(directory, exist_ok = True)
+
+            #Save figuer
+            plt.savefig(fname = save_path)
+
+            #Console outputs
+            print(f"Saved FFT Diagnostic to: {save_path}")
+
+        #Control flows for visualization
+        if visualize:
+            plt.show()
---
+++ src/generateSpeckle.py
@@ -0,0 +1,204 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import qmc
+from PIL import Image as im
+import os
+
+class generateSpeckle:
+
+    def __init__(self, width, height, speckle_size, blackwhite, run, mode):
+
+        """
+        Class to generate speckle patterns. 
+
+        Functions:
+        generateSpecklePositions    --> Seeds the speckles.
+        generateSpecklePattern      --> Creates the speckle pattern.
+        visualizeSpecklePattern     --> Self explanatory.
+
+        Inputs:
+        width, height, speckle_size --> Dimensions specified by user.
+        blackwhite                  --> Fraction of one colour to another.
+        run                         --> Run number.
+        mode                        --> Colour grading. 0 is black on white, 1 is white on black.
+        """
+
+        #Assign variables to self
+        self.width = width
+        self.height = height
+        self.speckle_size = speckle_size
+        self.blackwhite = blackwhite
+        self.run = run
+        self.mode = mode
+
+        #Colour information
+        if mode == 1:
+            self.specklecolor = "white"
+            self.backgroundcolor = "black"
+        elif mode == 0:
+            self.specklecolor = "black"
+            self.backgroundcolor = "white"
+        else:
+            raise ValueError("Mode value chosen is out of bounds. Chose either black on white or white on black!")
+
+        #Generate speckle seeds.
+        self.speckles = self.generateSpecklePositions()
+
+
+    #### Speckle Position Generator ####
+    def generateSpecklePositions(self):
+
+        """
+        generateSpecklePositions
+
+        function to generate the positions of the speckles. 
+        Uses halton sampling to create pointwise speckles, think of it as seed points. 
+
+        Outputs:
+        speckles --> 2D array of X,Y coordinates for speckles.
+        """
+
+        #Calculate total area in pixels.
+        area = self.width * self.height
+
+        #Calculate area that needs to be spanned by the speckles
+        self.speckle_radius = self.speckle_size/2
+        self.speckle_area = np.pi * (self.speckle_radius ** 2)
+
+        #Get the number of speckls needed
+        n_speckles = (area * self.blackwhite) / self.speckle_area
+        n_speckles = int(n_speckles) #Needs to be an integer
+
+        #Create the halton sampler
+        sampler = qmc.Halton(d = 2, optimization = "random-cd")
+        speckles = sampler.integers(n = n_speckles, l_bounds = [0,0], u_bounds = [self.width, self.height])
+
+        #Console output
+        print(f"Generated {n_speckles} speckle seeds.")
+
+        return speckles #This gives the pointwise locations of the speckles
+
+
+    #### Speckle Pattern Generator ####
+    def generateSpecklePattern(self, subdivisions = 1):
+
+        """
+        generateSpecklePattern
+
+        Takes pixel data from matplotlibs scatter to create a function for the speckle pattern. 
+        White pixels have the value 255 while black have the value 0. 
+        Can go to sub-pixel resolutions compared to the input pixel count.
+
+        Inputs:
+        subdivisions    --> How many splits inside a pixel are needed (resolution)
+
+        Outputs:
+        pattern         --> Dictionary containing all the information about the speckle pattern function
+                            Can be fed into diagnostics.
+        """
+
+        #Calculate required DPI
+        #For pattern creation: 500 pixels = 10 inches
+        x_inches = self.width * (10/500)
+        y_inches = self.height * (10/500)
+
+        #For subpixel accuracy we need:
+        target_pixels = self.width * subdivisions
+        #This gives us a required ppi/dpi of:
+        dpi = target_pixels/x_inches
+
+        #Create matplotlib scatter plot
+        fig, ax = plt.subplots()
+        fig.set_figheight(y_inches)
+        fig.set_figwidth(x_inches)
+        fig.set_dpi(dpi)
+        fig.patch.set_facecolor(self.backgroundcolor)
+        ax.set_facecolor(self.backgroundcolor)
+        ax.scatter(x = self.speckles[:,0], y = self.speckles[:,1], s = self.speckle_area, c = self.specklecolor)
+        ax.set_xlim((0, self.width))
+        ax.set_xscale("linear")
+        ax.set_ylim((0, self.height))
+        ax.set_xscale("linear")
+
+        #Taking the pixel maps from matplotlib
+        ax.axis('off')
+        fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+
+        fig.canvas.draw()
+
+        w, h = fig.canvas.get_width_height()
+        rgb_buffer = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
+        pixel_matrix_3d = rgb_buffer.reshape((h, w, 3))
+
+        #Make the grids and the function.
+        x = np.linspace(0, self.width, w)
+        y = np.linspace(0, self.height, h)
+        X,Y = np.meshgrid(x,y)
+        dx = self.width/w
+        dy = self.height/h
+
+        f = pixel_matrix_3d[:, :, 0]
+
+        #Save the X,Y grids and related information alongside the function f in a dictionary to send out.
+        pattern = {"f" : f, "X" : X, "dx" : dx, "nx" : w, "Y" : Y, "dy" : dy, "ny" : h}
+
+        #Console output
+        print("Finished creating the speckle pattern.")
+
+        return pattern
+
+
+    #### Speckle Visualizer ####
+    def visualizeSpecklePattern(self, visualize = True, save = True, filetype = ".tiff", ppi = 50):
+
+        """
+        visualizeSpecklePattern
+
+        Function to visualize or save the speckle pattern. 
+        Inputs:
+        visualize   --> Flag for visualization
+        save        --> Flag for saving
+        filetype    --> Filetype, can be either ".tiff" or ".bmp"
+        ppi         --> User specified pixels per inch if needed. Defaults to 50
+
+        Outputs:
+        Saved speckle pattern image.
+        """
+
+        #Get inch requirements:
+        x_inches = self.width * (10/500)
+        y_inches = self.height * (10/500)
+        
+        #Generate the pretty figure.
+        fig, ax = plt.subplots()
+        fig.set_dpi(ppi)
+        fig.set_figheight(y_inches)
+        fig.set_figwidth(x_inches)
+        fig.patch.set_facecolor("white")
+        ax.set_facecolor(self.backgroundcolor)
+        ax.scatter(x = self.speckles[:,0], y = self.speckles[:,1], s = self.speckle_area, c = self.specklecolor)
+        ax.set_xlim((0, self.width))
+        ax.set_xscale("linear")
+        ax.set_ylim((0, self.height))
+        ax.set_xscale("linear")
+        ax.set_title(f"Speckle pattern for run: {self.run}")
+
+        if save:
+            #File names
+            directory = "output/"+self.run+"/"
+            save_path = directory+self.run+"_SpecklePattern"+filetype
+            os.makedirs(directory, exist_ok = True)
+
+            #Render image
+            fig.canvas.draw()
+
+            #Saving as a .tiff file
+            image = im.frombytes("RGB", fig.canvas.get_width_height(), fig.canvas.tostring_rgb())
+            image.save(save_path)
+
+            #Console output
+            print(f"Speckle pattern saved as: {save_path}")
+
+        if visualize:
+            #Show image
+            plt.show()
